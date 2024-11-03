1696f9153963cf9b23e68f8f022f4c7ee9a6c6b3
Ammar AL-Hasanat <ammarabed13@gmail.com>
2024-11-03 23:45:19 +0300

    fixes

---
+++ AirlineReservationsSystem/Application/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using AirlineReservationsSystem.Domain.Entities;
+using AirlineReservationsSystem.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace AirlineReservationsSystem.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<SignInResult> LoginAsync(string email, string password, bool rememberMe);
+        Task<IdentityResult> RegisterAsync(User user, string password);
+        Task LogoutAsync();
+    }
+}
